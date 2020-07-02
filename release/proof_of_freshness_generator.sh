bd17566ff59ee7ede7e9d48d41f3dda5b5e38179
Patrick Schleizer <adrelanos@riseup.net>
2020-07-02 15:55:09 -0400

    reuters rss offline

--- release/proof_of_freshness_generator.sh
+++ release/proof_of_freshness_generator.sh
@@ -13,6 +13,6 @@ rsstail -1 -n5 -u https://rss.nytimes.com/services/xml/rss/nyt/World.xml
 
 rsstail -1 -n5 -u https://feeds.bbci.co.uk/news/world/rss.xml
 
-rsstail -1 -n5 -u http://feeds.reuters.com/reuters/worldnews
+#rsstail -1 -n5 -u http://feeds.reuters.com/reuters/worldnews
 
 curl --silent --fail --proto =https --tlsv1.2 https://blockchain.info/blocks/?format=json | python3 -c "import sys, json; print(json.load(sys.stdin)['blocks'][10]['hash'])"
