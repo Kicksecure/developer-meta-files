69d6ae357e0cbbb18cd9f47785013660356e83d5
Patrick Schleizer <adrelanos@riseup.net>
2020-10-06 08:27:45 -0400

    .

--- release/proof_of_freshness_generator.sh
+++ release/proof_of_freshness_generator.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## https://github.com/QubesOS/qubes-secpack/blob/master/utils/proof_of_freshness.py
+
 set -e
 
 export PS4="\n$ "
@@ -11,7 +13,7 @@ rsstail -1 -n5 -u https://www.spiegel.de/international/index.rss
 
 rsstail -1 -n5 -u https://rss.nytimes.com/services/xml/rss/nyt/World.xml
 
-rsstail -1 -n5 -u https://feeds.bbci.co.uk/news/world/rss.xml
+#rsstail -1 -n5 -u https://feeds.bbci.co.uk/news/world/rss.xml
 
 #rsstail -1 -n5 -u http://feeds.reuters.com/reuters/worldnews
 
